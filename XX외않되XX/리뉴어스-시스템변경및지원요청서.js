8591548e80e0557171a6368b725834796a71c56a
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-06-18 08:43:38 +0900

    Update 리뉴어스-시스템변경및지원요청서.js

--- XX외않되XX/리뉴어스-시스템변경및지원요청서.js
+++ XX외않되XX/리뉴어스-시스템변경및지원요청서.js
@@ -1,65 +1,47 @@
+checkInputSub: function () {
+    const inputEl = $("#tmp_title input");
+    let inputVal = inputEl.val();
 
-    checkInputSub: function () {
-        var title = "";
-        var title1 = "";
-        var title2 = "";
-        var title3 = "";
-        var selected = $(".selectedSub select").val();  // 선택된 대상시스템 드롭박스 옵션값
-        var request = $(".request select").val();  // 선택된 요청구분 드롭박스 옵션값
-        var emergency = $(".emergency select").val();  // 선택된 긴급여부 드롭박스 옵션값
-        
-        var inputVal = $("#tmp_title input").val();
-        var prefixes1 = $(".selectedSub select option").map(function () {   // 대상시스템 드롭박스 옵션값 모두 가져오기
-            return $(this).text();
-        }).get();
-        var prefixes2 = $(".request select option").map(function () {   // 요청구분 드롭박스 옵션값 모두 가져오기
-            return $(this).text();
-        }).get();
-        var prefixes3 = $(".emergency select option").map(function () {   // 긴급여부 드롭박스 옵션값 모두 가져오기
+    // 접두어 제거 함수
+    function removePrefixes(val, selector, delimiter) {
+        const prefixes = $(`${selector} option`).map(function () {
             return $(this).text();
         }).get();
 
-        prefixes1.forEach(function (prefix) {
-            if (inputVal.startsWith(prefix + "/")) {
-                inputVal = inputVal.slice(prefix.length + 1); // prefix 제거
+        prefixes.forEach(function (prefix) {
+            if (val.startsWith(prefix + delimiter)) {
+                val = val.slice(prefix.length + delimiter.length);
             }
         });
-        prefixes2.forEach(function (prefix) {
-            if (inputVal.startsWith(prefix + "/")) {
-                inputVal = inputVal.slice(prefix.length + 1); // prefix 제거
-            }
-        });
-        prefixes3.forEach(function (prefix) {
-            if (inputVal.startsWith(prefix + "_")) {
-                inputVal = inputVal.slice(prefix.length + 1); // prefix 제거
-            }
-        });
-    
-        if (selected && selected !== "--선택--") {
-            title1 = selected + "/";
-            title = title1 + title2 + title3;
-            $("#prefixText").text(title);
-        }
-        if (request && request !== "--선택--") {
-            title2 = request + "/";
-            title = title1 + title2 + title3;
-            $("#prefixText").text(title);
-        }
-        if (emergency && emergency !== "--선택--") {
-            title3 = emergency + "_";
-            title = title1 + title2 + title3;
-            $("#prefixText").text(title);
-        }
-        if (selected == "--선택--") {
-            title1 = "";
-            title = title1 + title2 + title3;
-            $("#prefixText").text(title);
-        }
-        if (request == "--선택--") {
-            title2 = "";
-            title = title1 + title2 + title3;
-            $("#prefixText").text(title);
-        }
 
-        $("#tmp_title input").val(inputVal);
-    },
+        return val;
+    }
+
+    // 접두사 제거
+    inputVal = removePrefixes(inputVal, ".selectedSub select", "/");
+    inputVal = removePrefixes(inputVal, ".request select", "/");
+    inputVal = removePrefixes(inputVal, ".emergency select", "_");
+
+    // 접두사 생성
+    const selected = $(".selectedSub select").val();
+    const request = $(".request select").val();
+    const emergency = $(".emergency select").val();
+
+    const titleParts = [];
+
+    if (selected && selected !== "--선택--") {
+        titleParts.push(selected + "/");
+    }
+
+    if (request && request !== "--선택--") {
+        titleParts.push(request + "/");
+    }
+
+    if (emergency && emergency !== "--선택--") {
+        titleParts.push(emergency + "_");
+    }
+
+    const title = titleParts.join("");
+    $("#prefixText").text(title);
+    inputEl.val(inputVal);
+},
