32fa54f263298918881732d1a81a6a24bcead359
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-06-19 05:42:30 +0000

    Update 피아이이-경조금.js

--- XX외않되XX/피아이이-경조금.js
+++ XX외않되XX/피아이이-경조금.js
@@ -1,56 +1,36 @@
-var $ = require("jquery");
-var app = require("app");
-var Backbone = require("backbone");
-var _ = require('underscore');
+checkInputSub: function () {
+    var title = "";
+    var selected = $(".selectedSub select").val();
+    var subRel = $(".subRel input").val();
+    var subName = $(".subName input").val();
+    var occDate = this.formatDate($(".occDate input").val());
 
-
-var Integration = Backbone.View.extend({
-  initialize : function(options){
-        this.options = options || {};
-        this.docModel = this.options.docModel;
-        this.variables = this.options.variables;
-        this.infoData = this.options.infoData;
-    },
-
-    render : function() {
-        var self = this;
-        $('.viewModeHiddenPart').show();
-
-        $(".selectedSub select, .occDate input").on("change", function () {
-            self.checkInputSub();
-        });
-    },	
-
-    checkInputSub: function () {
-      var title = "";
-      var selected = $(".selectedSub select").val();
-      var subRel = $(".subRel input").val();
-      var subName = $(".subName input").val();
-      var occDate = $(".occDate input").val();
-  
-      if (subRel || subName || occDate) {
-          title = selected + "(" + subRel + " " + subName + ")" + occDate;
-          $("#subject input").val(title);
-          $("#tmp_title input").val(title);
-      }
-    },
-
-
-    renderViewMode : function(){$('.viewModeHiddenPart').hide();},
-    onEditDocument : function(){this.render();},
-    beforeSave :function() {
-        $('.viewModeHiddenPart').hide();
-
-        var selected = $(".selectedSub select").val();
-        var subRel = $(".subRel input").val();
-        var subName = $(".subName input").val();
-        var occDate = $(".occDate input").val();
-
+    if (subRel || subName || occDate) {
         title = selected + "(" + subRel + " " + subName + ")" + occDate;
+        $("#subject input").val(title);
+        $("#tmp_title input").val(title);
+    }
+},
 
-        $("#subject").val(title); // 최종 제목 세팅
-    },
-    afterSave :function() {$('.viewModeHiddenPart').hide();},
-    validate :function() {return true;},getDocVariables : function(){}
-});
-return Integration;
+beforeSave : function() {
+    $('.viewModeHiddenPart').hide();
+
+    var selected = $(".selectedSub select").val();
+    var subRel = $(".subRel input").val();
+    var subName = $(".subName input").val();
+    var occDate = this.formatDate($(".occDate input").val());
+
+    var title = selected + "(" + subRel + " " + subName + ")" + occDate;
+
+    $("#subject").val(title);
+},
+
+formatDate: function(rawDate) {
+    var date = new Date(rawDate);
+    if (isNaN(date)) return rawDate;
+
+    var yyyy = date.getFullYear();
+    var mm = ("0" + (date.getMonth() + 1)).slice(-2);
+    var dd = ("0" + date.getDate()).slice(-2);
+    return `${yyyy}-${mm}-${dd}`;
+}
