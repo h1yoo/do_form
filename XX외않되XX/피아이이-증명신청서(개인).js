5916391211725ffc57d9272fd353af5e9792b6c1
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-06-19 15:44:08 +0900

    Update 피아이이-증명신청서(개인).js

--- XX외않되XX/피아이이-증명신청서(개인).js
+++ XX외않되XX/피아이이-증명신청서(개인).js
@@ -1,49 +1,10 @@
 
-checkInputSub: function () {
-    var subCertSel = [];
-    
-    $(".subCertSel").nextAll("tr").slice(0, 3).find("td").each(function () {
-        var $td = $(this);
-        var label = $td.contents().filter(function () {
-            return this.nodeType === 3; // 텍스트 노드만
-        }).text().trim();
-
-        var value = $td.find("select").val();
-        if (value !== "0") {
-            subCertSel.push(label);
-        }
-    });
-
-    var subDate = this.formatDate($(".subDate input").val());
-    var subUse = $(".subUse select").val();
-
-    if (subCertSel.length || subDate || subUse) {
-        var title = subUse + "(" + subDate + ", " + subCertSel.join(", ") + ")" || "";
-        $("#subject input").val(title);
-        $("#tmp_title input").val(title);
-    }
-},
-
-beforeSave: function () {
-    $('.viewModeHiddenPart').hide();
-
-    var subCertSel = [];
-    
-    $(".subCertSel").nextAll("tr").slice(0, 3).find("td").each(function () {
-        var $td = $(this);
-        var label = $td.contents().filter(function () {
-            return this.nodeType === 3;
-        }).text().trim();
-
-        var value = $td.find("select").val();
-        if (value !== "0") {
-            subCertSel.push(label);
-        }
-    });
-
-    var subDate = this.formatDate($(".subDate input").val());
-    var subUse = $(".subUse select").val();
-
-    var title = subUse + "(" + subDate + ", " + subCertSel.join(", ") + ")" || "";
-    $("#subject").val(title);
-}
+// 가장 안전하고 깔끔한 방법: 전체 테이블/섹션 범위에서 체크된 것들 모두 수집
+var subCertSel = $("table .subCertSel")
+    .closest("table") // 같은 테이블 안에서
+    .find("input[type='checkbox']:checked")
+    .map(function () {
+        return $(this).val();
+    })
+    .get()
+    .join(", ");
