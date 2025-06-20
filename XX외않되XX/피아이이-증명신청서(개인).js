8731e23bea5b531e0d81d08487fbad86b5f92d46
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-06-20 10:15:14 +0900

    Update 피아이이-증명신청서(개인).js

--- XX외않되XX/피아이이-증명신청서(개인).js
+++ XX외않되XX/피아이이-증명신청서(개인).js
@@ -1,37 +1,48 @@
-const certTypes = [
-  '재직증명',
-  '재직증명(영문)',
-  '경력증명',
-  '경력증명(영문)',
-  '원천징수영수증',
-  '갑종근로소득납세필증명'
-];
 
-const certCounts = Array(certTypes.length).fill(0);
+    checkInputSub: function () {
+        const certTypes = [
+            '재직증명',
+            '재직증명(영문)',
+            '경력증명',
+            '경력증명(영문)',
+            '원천징수영수증',
+            '갑종근로소득납세필증명'
+        ];
+        
+        const certCounts = Array(certTypes.length).fill(0);
+        
+        $('td').each(function () {
+            const td = $(this);
+        
+            certTypes.forEach(function (certName, index) {
+            // td 안의 span에 텍스트가 있는지 확인
+            const span = td.find('span');
+            if (span.length && span.text().trim() === certName) {
+                // 같은 td 안에 select가 있는 경우
+                const select = td.find('select.editor_slt');
+                if (select.length > 0) {
+                const val = parseInt(select.val(), 10);
+                if (!isNaN(val)) {
+                    certCounts[index] = val;
+                }
+                }
+        
+                // 또는 체크박스가 체크되었는지 확인
+                const checkbox = td.find('input[type="checkbox"]');
+                if (checkbox.length && checkbox.is(':checked')) {
+                certCounts[index] = 1; // 체크됐을 때 1로 표시
+                }
+            }
+            });
+        });
+        
+        console.log(certCounts);
+        var subDate = this.formatDate($(".subDate input").val());
+        var subUse = $(".subUse select").val();
 
-$('td').each(function () {
-  const td = $(this);
-
-  certTypes.forEach(function (certName, index) {
-    // td 안의 span에 텍스트가 있는지 확인
-    const span = td.find('span');
-    if (span.length && span.text().trim() === certName) {
-      // 같은 td 안에 select가 있는 경우
-      const select = td.find('select.editor_slt');
-      if (select.length > 0) {
-        const val = parseInt(select.val(), 10);
-        if (!isNaN(val)) {
-          certCounts[index] = val;
+        if (subCertSel || subDate || subUse) {
+                var title = subUse + "(" + subDate + ", " + subCertSel + ")" || "";
+                $("#subject input").val(title);
+                $("#tmp_title input").val(title);
         }
-      }
-
-      // 또는 체크박스가 체크되었는지 확인
-      const checkbox = td.find('input[type="checkbox"]');
-      if (checkbox.length && checkbox.is(':checked')) {
-        certCounts[index] = 1; // 체크됐을 때 1로 표시
-      }
-    }
-  });
-});
-
-console.log(certCounts);
+    },
