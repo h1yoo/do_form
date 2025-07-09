c338fc4d5e8f6ba5abb0c303e41f57814c04d2fd
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-07-09 14:44:50 +0900

    Add files via upload

---
+++ js/viewModeHiddenPart.js
@@ -0,0 +1,27 @@
+var $ = require("jquery");
+var app = require("app");
+var Backbone = require("backbone");
+var _ = require('underscore');
+
+
+var Integration = Backbone.View.extend({
+	initialize : function(options){
+		this.options = options || {};
+		this.docModel = this.options.docModel;
+		this.variables = this.options.variables;
+		this.infoData = this.options.infoData;
+	},
+	
+	render : function() {
+		var self = this;
+		$('.viewModeHiddenPart').show();
+	},
+
+	renderViewMode : function(){$('.viewModeHiddenPart').hide();},
+	onEditDocument : function(){this.render();},
+	beforeSave :function() {$('.viewModeHiddenPart').hide();},
+	afterSave :function() {$('.viewModeHiddenPart').hide();},
+	validate :function() {return true;},
+	getDocVariables : function(){}
+});
+return Integration;
