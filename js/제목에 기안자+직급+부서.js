535a5df97b3b03490ddca36c8ee92f4997f29538
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-07-25 08:58:35 +0900

    Update 제목에 기안자+직급+부서.js

--- js/제목에 기안자+직급+부서.js
+++ js/제목에 기안자+직급+부서.js
@@ -5,156 +5,146 @@ var _ = require('underscore');
 
 var Integration = Backbone.View.extend({
 
-    initialize : function(options) {
-    
-    this.options = options || {};
-    
-    this.docModel = this.options.docModel;
-    
-    this.variables = this.options.variables;
-    
-    },
-    
-     
-    
-    render : function() {
-    
-    var self = this;
-    
-    if (this.docModel.docStatus == 'CREATE' && this.docModel.docType == 'DRAFT') {
-        
-        self.autoSubject();
-    
-    }
-    
-     
-    
-    if (GO.util.store.get('document.docMode') == 'EDIT') {
-    
-    self.setEvent();
-    
-    }
-    
-    },
-
-
-     // 제목 컴포넌트에 표시하기 위해 autoSubject 함수 추가
-	// 이 부분만 추가하고, render 부분에 self.autoSubject(); 추가해서 사용하면 됨
-	// html 건드릴 필요는 없지만 #draftDept가 안먹으면 기안부서 td 태그에 userDep 클래스 추가해서 사용하기
-    autoSubject: function() {
-		$('.sign_type1_inline').each(function (index2, item) {
-			$(item).find('.sign_member').each(function (index, item) {
-                var sign_name = $(item).find('.sign_name').text();  // 기안자
-                var sign_rank = $(item).find('.sign_rank').text();  // 직급
-                // var draftDept = $('.userDep').find('input').val();  // 기안부서
-                var draftDept = $('#draftDept').val();  // 기안부서
-                
-                $('#subject').val(sign_name+ " " + sign_rank + " " + draftDept);
-			});
-
-		});
-    },
-    
-     
-    
-    setEvent : function() {
-    
-    var self = this;
-    
-    },
-    
-     
-    
-    renderViewMode : function() {
-    
-    $('.viewModeHiddenPart').hide();
-    
-    },
-    
-     
-    
-    onEditDocument : function() {
-    
-    this.render();
-    
-    },
-    
-     
-    
-    beforeSave : function() {
-    
-    },
-    
-     
-    
-    afterSave : function() {
-    
-    },
-    
-     
-    
-    nvlToEmpty : function(v) {
-    
-    if (v) {
-    
-    return _.isNull(v) ? "" : v;
-    
-    }
-    
-    return "";
-    
-    },
-    
-     
-    
-     
-    
-     
-    validate : function() {
-    
-    var self = this;
-    
-               try {
-    
-                self.finalSignCheck();
-    
-                   return true;
-    
-                 } catch (error) {
-    
-                $.goMessage(error.message)
-    
-                   return false;
-    
-                 }
-    
-    },
-    
-    
-    // 최종결재자 유무 판단
-    
-    finalSignCheck: function() {
-    
-      if($('.sign_type1_inline').eq(2).find('.sign_member').text() == ""){
+  initialize : function(options) {
+  
+  this.options = options || {};
+  
+  this.docModel = this.options.docModel;
+  
+  this.variables = this.options.variables;
+  
+  },
+  
+   
+  render : function() {
+  
+  var self = this;
+  
+  if (this.docModel.docStatus == 'CREATE' && this.docModel.docType == 'DRAFT') {
       
-      throw new Error("최종 결재자 필수 추가");
-      
-      }
+      self.autoSubject();
+  
+  }
+  
+  
+  if (GO.util.store.get('document.docMode') == 'EDIT') {
+  
+  self.setEvent();
+  
+  }
+  
+  },
+  
+   
+  autoSubject: function() {
+    // // html 그대로 사용 시 >> 결재 라인에 기안자 본인이 없으면 제목에 안나타남 >> html 수정 필요
+    // $('.sign_type1_inline').each(function (index2, item) {
+    //   $(item).find('.sign_member').each(function (index, item) {
+    //     var sign_name = $(item).find('.sign_name').text();  // 기안자
+    //     var sign_rank = $(item).find('.sign_rank').text();  // 직급
+    //     var draftDept = $('#draftDept').val();  // 기안부서
+    //     $('#subject').val(sign_name+ " " + sign_rank + " " + draftDept);
+    //   });
+    // });
+
+    // html 수정 시
+    var userName = $('.userName').find('input').val();  // 기안자
+    var userRank = $('.userRank').find('input').val();  // 직급
+    var userDep = $('.userDep').find('input').val();  // 기안부서
+    $('#subject').val(userName+ " " + userRank + " " + userDep);
     
-     
+  },
+  
+   
+  
+  setEvent : function() {
+  
+  var self = this;
+  
+  },
+  
+   
+  
+  renderViewMode : function() {
+  
+  $('.viewModeHiddenPart').hide();
+  
+  },
+  
+   
+  
+  onEditDocument : function() {
+  
+  this.render();
+  
+  },
+  
+   
+  beforeSave : function() {
+  
+  },
+  
+  
+  afterSave : function() {
+  
+  },
+  
+   
+  nvlToEmpty : function(v) {
+  
+  if (v) {
+  
+  return _.isNull(v) ? "" : v;
+  
+  }
+  
+  return "";
+  
+  },
+  
+   
+  validate : function() {
+  
+  var self = this;
+  
+             try {
+  
+              self.finalSignCheck();
+  
+                 return true;
+  
+               } catch (error) {
+  
+              $.goMessage(error.message)
+  
+                 return false;
+  
+               }
+  
+  },
+  
+  
+  // 최종결재자 유무 판단
+  
+  finalSignCheck: function() {
+  
+    if($('.sign_type1_inline').eq(2).find('.sign_member').text() == ""){
     
-    },
-    
-    getDocVariables : function() {
-    
-    return _.extend(this.variables, {});
+    throw new Error("최종 결재자 필수 추가");
     
     }
-    
-    });
-    
-     
-    
-     
-    
-    return Integration;
+  
+   
+  
+  },
+  
+  getDocVariables : function() {
+  
+  return _.extend(this.variables, {});
+  
+  }
+  
+  });
+   
+  
+  return Integration;
