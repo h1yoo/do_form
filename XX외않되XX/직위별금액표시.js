1d042420a036b3928359b8098da6b04babaaa377
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-07-28 08:49:56 +0900

    Update 직위별금액표시.js

--- XX외않되XX/직위별금액표시.js
+++ XX외않되XX/직위별금액표시.js
@@ -4,19 +4,19 @@
 		$("#dynamic_table4 tr").each(function(i, e){
       if ($(e).find(".userRank select")[0]) {
         var selectedUserRank = $('.userRank select option:selected').val();
-        var price = 0;
+        var price = "";
 
         if(selectedUserRank == "주임" || selectedUserRank == "대리" || selectedUserRank == "과장" || selectedUserRank == "차장"){
-          price = 30000;
+          price = "30000";
         }
         else if(selectedUserRank == "팀장"){
-          price = 35000;
+          price = "35000";
         }
         else if(selectedUserRank == "실장" || selectedUserRank == "위원"){
-          price = 40000;
+          price = "40000";
         }
         else if(selectedUserRank == "대표" || selectedUserRank == "소장" || selectedUserRank == "본부장" || selectedUserRank == "이사"){
-          price = 50000;
+          price = "50000";
         }
         else {
           price = "";
