eddca6159d724aa0fd5d00cee9a0551d5b2b9c76
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-07-28 09:55:11 +0900

    Update 직위별금액표시.js

--- XX외않되XX/직위별금액표시.js
+++ XX외않되XX/직위별금액표시.js
@@ -1,43 +1,34 @@
+calDayPrice: function () {
+  // 일비 정산금액 계산
+  $("#dynamic_table4 tr").each(function (i, e) {
+    var selectedUserRank = $(e).find(".userRank select option:selected").val();
 
-  calDayPrice : function () {
-    // 일비 정산금액 계산
-		$("#dynamic_table4 tr").each(function(i, e){
-      if ($(e).find(".userRank select option:selected")[0]) {
-        var selectedUserRank = $('.userRank select option:selected').val();
-        // var price = "";
+    if (selectedUserRank) {
+      let price = 0;
 
-        if(selectedUserRank == "주임" || selectedUserRank == "대리" || selectedUserRank == "과장" || selectedUserRank == "차장"){
-          // price = "30000";
-          $(e).find(".price4").text(GO.util.numberWithCommas("30000"));
-        }
-        else if(selectedUserRank == "팀장"){
-          // price = "35000";
-          $(e).find(".price4").text(GO.util.numberWithCommas("35000"));
-        }
-        else if(selectedUserRank == "실장" || selectedUserRank == "위원"){
-          // price = "40000";
-          $(e).find(".price4").text(GO.util.numberWithCommas("40000"));
-        }
-        else if(selectedUserRank == "대표" || selectedUserRank == "소장" || selectedUserRank == "본부장" || selectedUserRank == "이사"){
-          // price = "50000";
-          $(e).find(".price4").text(GO.util.numberWithCommas("50000"));
-        }
-        else {
-          price = "";
-        }
-
-        // $(e).find(".price4").text(GO.util.numberWithCommas(price));
+      if (["주임", "대리", "과장", "차장"].includes(selectedUserRank)) {
+        price = 30000;
+      } else if (selectedUserRank === "팀장") {
+        price = 35000;
+      } else if (["실장", "위원"].includes(selectedUserRank)) {
+        price = 40000;
+      } else if (["대표", "소장", "본부장", "이사"].includes(selectedUserRank)) {
+        price = 50000;
       }
-    });
 
-    var sum_price4 = 0;
+      $(e).find(".price4").text(price ? GO.util.numberWithCommas(price) : "");
+    }
+  });
 
-    $(".price4").each(function () {
-      var val = parseFloat($(this).text().replace(/,/g, ""));
-      if (!isNaN(val)) {
-        sum_price4 += val;
-      }
-    });
+  // 총 합계 계산
+  var sum_price4 = 0;
 
-    $(".sum_price4").text(GO.util.numberWithCommas(sum_price4));
-  },
+  $(".price4").each(function () {
+    var val = parseFloat($(this).text().replace(/,/g, ""));
+    if (!isNaN(val)) {
+      sum_price4 += val;
+    }
+  });
+
+  $(".sum_price4").text(GO.util.numberWithCommas(sum_price4));
+}
