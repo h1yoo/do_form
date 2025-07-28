f375afb225b9ac824d886b03f0eb8bc26401b7e1
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-07-28 09:50:21 +0900

    Update 직위별금액표시.js

--- XX외않되XX/직위별금액표시.js
+++ XX외않되XX/직위별금액표시.js
@@ -2,27 +2,42 @@
   calDayPrice : function () {
     // 일비 정산금액 계산
 		$("#dynamic_table4 tr").each(function(i, e){
-      if ($(e).find(".userRank select")[0]) {
+      if ($(e).find(".userRank select option:selected")[0]) {
         var selectedUserRank = $('.userRank select option:selected').val();
-        var price = "";
+        // var price = "";
 
         if(selectedUserRank == "주임" || selectedUserRank == "대리" || selectedUserRank == "과장" || selectedUserRank == "차장"){
-          price = "30000";
+          // price = "30000";
+          $(e).find(".price4").text(GO.util.numberWithCommas("30000"));
         }
         else if(selectedUserRank == "팀장"){
-          price = "35000";
+          // price = "35000";
+          $(e).find(".price4").text(GO.util.numberWithCommas("35000"));
         }
         else if(selectedUserRank == "실장" || selectedUserRank == "위원"){
-          price = "40000";
+          // price = "40000";
+          $(e).find(".price4").text(GO.util.numberWithCommas("40000"));
         }
         else if(selectedUserRank == "대표" || selectedUserRank == "소장" || selectedUserRank == "본부장" || selectedUserRank == "이사"){
-          price = "50000";
+          // price = "50000";
+          $(e).find(".price4").text(GO.util.numberWithCommas("50000"));
         }
         else {
           price = "";
         }
 
-        $(e).find(".price4").text(GO.util.numberWithCommas(calWon.toFixed(0)));
+        // $(e).find(".price4").text(GO.util.numberWithCommas(price));
       }
     });
+
+    var sum_price4 = 0;
+
+    $(".price4").each(function () {
+      var val = parseFloat($(this).text().replace(/,/g, ""));
+      if (!isNaN(val)) {
+        sum_price4 += val;
+      }
+    });
+
+    $(".sum_price4").text(GO.util.numberWithCommas(sum_price4));
   },
