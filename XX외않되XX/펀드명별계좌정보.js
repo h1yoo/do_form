8509d4ea5d866ef100e9f65b494e5ed840cdfa0c
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-07-28 13:11:41 +0900

    Update 펀드명별계좌정보.js

--- XX외않되XX/펀드명별계좌정보.js
+++ XX외않되XX/펀드명별계좌정보.js
@@ -1,15 +1,10 @@
-$firstCBRERow.find('.out1 select').append('<option value="(주)하나은행">(주)하나은행</option>');
+
 			
-
-
-
-
-
-$('#select1').on('change', function(){
-			
-			$('#text span').hide();
-			$('#stmap span').hide();
 			var select1 = $('#select1 select option:selected').val();
+      // var firstCBRErow = $('[class^="CBRE_"]').first();
+      // firstCBRErow.find('.out1 select').append('\
+      //   <option value="(주)하나은행">(주)하나은행</option>\
+      //   ');
       
       if(select1 == "CBRE GSA로지스틱스제1호전문투자형사모부동산자투자유한회사"){
           $('#add2').show();
@@ -19,19 +14,19 @@ $('#select1').on('change', function(){
 
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
           $('.CBRE_GSA1_Acc').show();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
 
         }else if(select1 == "CBRE GSA로지스틱스제1의1호전문투자형사모부동산모투자유한회사"){
           $('#add3').show();
@@ -40,20 +35,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
           $('.CBRE_GSA1_1_Acc').show();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE GSA로지스틱스제1의2호전문투자형사모부동산모투자유한회사"){
           $('#add4').show();
@@ -62,20 +57,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
           $('.CBRE_GSA1_2_Acc').show();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE GSA로지스틱스제1의3호전문투자형사모부동산모투자유한회사"){
           $('#add5').show();
@@ -84,20 +79,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
           $('.CBRE_GSA1_3_Acc').show();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE GSA로지스틱스제1의4호전문투자형사모부동산모투자유한회사"){
           $('#add6').show();
@@ -106,20 +101,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
           $('.CBRE_GSA1_4_Acc').show();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE GSA로지스틱스제1의5호전문투자형사모부동산모투자유한회사"){
           $('#add7').show();
@@ -128,20 +123,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
           $('.CBRE_GSA1_5_Acc').show();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE 로지스밸리제1의6호전문투자형사모부동산모투자유한회사"){
           $('#add8').show();
@@ -150,20 +145,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
           $('.CBRE_GSA1_6_Acc').show();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE GSA로지스틱스제2호전문투자형사모부동산자투자유한회사"){
           $('#add9').show();
@@ -172,20 +167,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
           $('.CBRE_GSA2_Acc').show();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else if(select1 == "CBRE 로지스밸리전문투자형사모부동산투자신탁제1호"){
           $('#add11').show();
@@ -194,20 +189,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('(주)KB펀드파트너스');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').show();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_1_Acc').show();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
 
       }
       else if(select1 == "CBRE ILC전문투자형사모부동산자투자신탁제1호"){
@@ -217,20 +212,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').show();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_ILC1_Acc').show();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
 
       }else if(select1 == "CBRE ILC일반사모부동산모투자신탁제1-1호"){
           $('#add13').show();
@@ -239,20 +234,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').show();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_ILC1_1_Acc').show();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_defaultInput').hide();
 
       }else if(select1 == "CBRE DL일반부동산사모투자회사"){
           $('#add10').show();
@@ -261,20 +256,20 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').hide();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').show();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_DL_Acc').show();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_ILC2_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_defaultInput').hide();
 
       }else if(select1 == "CBRE ILC일반사모부동산자투자신탁제2호"){
           $('#add11').show();
@@ -283,31 +278,36 @@ $('#select1').on('change', function(){
           $('.out2 select').val('삼정KPMG AAS Inc.');  // 일반사무관리회사 옵션값 변경
           
           // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-          $('.CBRE_GSA1_Acc').hide();
-          $('.CBRE_GSA1_1_Acc').hide();
-          $('.CBRE_GSA1_2_Acc').hide();
-          $('.CBRE_GSA1_3_Acc').hide();
-          $('.CBRE_GSA1_4_Acc').hide();
-          $('.CBRE_GSA1_5_Acc').hide();
-          $('.CBRE_GSA1_6_Acc').hide();
-          $('.CBRE_GSA2_Acc').hide();
-          $('.CBRE_ILC_1').hide();
-          $('.CBRE_ILC_2').show();
-          $('.CBRE_ILC_1_1').hide();
-          $('.CBRE_1').hide();
-          $('.CBRE_DL').hide();
-          $('.CBRE_defaoulInput').hide();
+          $('.CBRE_ILC2_Acc').show();
+          $('.CBRE_GSA1_Acc,\
+             .CBRE_GSA1_1_Acc,\
+             .CBRE_GSA1_2_Acc,\
+             .CBRE_GSA1_3_Acc,\
+             .CBRE_GSA1_4_Acc,\
+             .CBRE_GSA1_5_Acc,\
+             .CBRE_GSA1_6_Acc,\
+             .CBRE_GSA2_Acc,\
+             .CBRE_1_Acc,\
+             .CBRE_DL_Acc,\
+             .CBRE_ILC1_Acc,\
+             .CBRE_ILC1_1_Acc,\
+             .CBRE_defaultInput').hide();
           
       }else{
         // 펀드명 별 수취계좌 & 입금/출금 계좌 설정
-        $('.CBRE_GSA1_Acc').hide();
-        $('.CBRE_GSA1_1_Acc').hide();
-        $('.CBRE_GSA1_2_Acc').hide();
-        $('.CBRE_GSA1_3_Acc').hide();
-        $('.CBRE_GSA1_4_Acc').hide();
-        $('.CBRE_GSA1_5_Acc').hide();
-        $('.CBRE_GSA1_6_Acc').hide();
-        $('.CBRE_GSA2_Acc').hide();
-        $('.CBRE_defaoulInput').show();
+        $('.CBRE_defaultInput').show();
+        $('.CBRE_GSA1_Acc,\
+           .CBRE_GSA1_1_Acc,\
+           .CBRE_GSA1_2_Acc,\
+           .CBRE_GSA1_3_Acc,\
+           .CBRE_GSA1_4_Acc,\
+           .CBRE_GSA1_5_Acc,\
+           .CBRE_GSA1_6_Acc,\
+           .CBRE_GSA2_Acc,\
+           .CBRE_1_Acc,\
+           .CBRE_DL_Acc,\
+           .CBRE_ILC1_Acc,\
+           .CBRE_ILC1_1_Acc,\
+           .CBRE_ILC2_Acc').hide();
 			}
     });
