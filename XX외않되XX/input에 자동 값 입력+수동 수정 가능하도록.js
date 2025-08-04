7caf458b914fd4f62bc2b3b61388ba32fb4ba142
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-08-05 08:30:59 +0900

    Update input에 자동 값 입력+수동 수정 가능하도록.js

--- XX외않되XX/input에 자동 값 입력+수동 수정 가능하도록.js
+++ XX외않되XX/input에 자동 값 입력+수동 수정 가능하도록.js
@@ -1,18 +1,12 @@
 
   calVatTotalAMT : function () {
     $("#dynamic_table1 tr").each(function(i, e){
+
       if ($(e).find(".netAMT")[0]) {
         var netAMT = parseFloat($(e).find(".netAMT input").val().replace(/,/g, "")) || 0;
         var vat = parseFloat($(e).find(".vat input").val().replace(/,/g, "")) || netAMT * 0.16;
         var WithholdTax = parseFloat($(e).find(".WithholdTax input").val().replace(/,/g, "")) || 0;
-        var totalAMT = parseFloat($(e).find(".totalAMT input").val().replace(/,/g, ""));
-        if (WithholdTax <= 0) {
-          var copy_totalAMT = netAMT + vat;
-          $(e).find(".totalAMT input").val(GO.util.numberWithCommas(copy_totalAMT.toFixed(2)));
-        } else {
-          var copy_totalAMT = netAMT + vat - WithholdTax;
-          $(e).find(".totalAMT input").val(GO.util.numberWithCommas(copy_totalAMT.toFixed(2)));
-        }
+        var totalAMT = parseFloat($(e).find(".totalAMT input").val().replace(/,/g, "")) || netAMT + vat - WithholdTax;
         
         $(e).find(".vat input").val(GO.util.numberWithCommas(vat.toFixed(2)));
         $(e).find(".totalAMT input").val(GO.util.numberWithCommas(totalAMT.toFixed(2)));
