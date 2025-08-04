b4cf3021fce5a9d3e3aa84513b39c93bdfaf24b1
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-08-04 15:38:40 +0900

    Update input에 자동 값 입력+수동 수정 가능하도록.js

--- XX외않되XX/input에 자동 값 입력+수동 수정 가능하도록.js
+++ XX외않되XX/input에 자동 값 입력+수동 수정 가능하도록.js
@@ -1,34 +1,14 @@
-(function () {
-  // 수동 입력 여부 추적
-  $(document).on("input", ".vat input", function () {
-    $(this).data("isUserEdited", true);
-  });
 
-  $(document).on("input", ".netAMT input", function () {
-    var $tr = $(this).closest("tr");
-    var $vatInput = $tr.find(".vat input");
-
-    // netAMT가 바뀌면 수동 상태 해제
-    $vatInput.data("isUserEdited", false);
-
-    // 즉시 자동 계산
-    GO.fn.calVat();
-  });
-
-  // VAT 계산 함수
-  GO.fn.calVat = function () {
-    $("#dynamic_table1 tr").each(function (i, e) {
-      var $row = $(e);
-      var $netInput = $row.find(".netAMT input");
-      var $vatInput = $row.find(".vat input");
-
-      var netAMT = parseFloat($netInput.val().replace(/,/g, "")) || 0;
-      var isUserEdited = $vatInput.data("isUserEdited");
-
-      if (!isUserEdited) {
-        var vat = netAMT * 0.16;
-        $vatInput.val(GO.util.numberWithCommas(vat.toFixed(2)));
+  calVatTotalAMT : function () {
+    $("#dynamic_table1 tr").each(function(i, e){
+      if ($(e).find(".netAMT")[0]) {
+        var netAMT = parseFloat($(e).find(".netAMT input").val().replace(/,/g, "")) || 0;
+        var vat = parseFloat($(e).find(".vat input").val().replace(/,/g, "")) || netAMT * 0.16;
+        var WithholdTax = parseFloat($(e).find(".WithholdTax input").val().replace(/,/g, "")) || 0;
+        var totalAMT = parseFloat($(e).find(".totalAMT input").val().replace(/,/g, "")) || netAMT + vat - WithholdTax;
+        
+        $(e).find(".vat input").val(GO.util.numberWithCommas(vat.toFixed(2)));
+        $(e).find(".totalAMT input").val(GO.util.numberWithCommas(totalAMT.toFixed(2)));
       }
     });
-  };
-})();
+  },
