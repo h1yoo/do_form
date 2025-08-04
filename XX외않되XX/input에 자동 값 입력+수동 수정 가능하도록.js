853518a01c5e8a51574d4b6c3abd3f92c0b3137c
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-08-04 15:32:22 +0900

    Update input에 자동 값 입력+수동 수정 가능하도록.js

--- XX외않되XX/input에 자동 값 입력+수동 수정 가능하도록.js
+++ XX외않되XX/input에 자동 값 입력+수동 수정 가능하도록.js
@@ -1,11 +1,34 @@
+(function () {
+  // 수동 입력 여부 추적
+  $(document).on("input", ".vat input", function () {
+    $(this).data("isUserEdited", true);
+  });
 
-  calVat : function () {
-    $("#dynamic_table1 tr").each(function(i, e){
-      if ($(e).find(".netAMT")[0]) {
-        var netAMT = parseFloat($(e).find(".netAMT input").val().replace(/,/g, "")) || 0;
+  $(document).on("input", ".netAMT input", function () {
+    var $tr = $(this).closest("tr");
+    var $vatInput = $tr.find(".vat input");
+
+    // netAMT가 바뀌면 수동 상태 해제
+    $vatInput.data("isUserEdited", false);
+
+    // 즉시 자동 계산
+    GO.fn.calVat();
+  });
+
+  // VAT 계산 함수
+  GO.fn.calVat = function () {
+    $("#dynamic_table1 tr").each(function (i, e) {
+      var $row = $(e);
+      var $netInput = $row.find(".netAMT input");
+      var $vatInput = $row.find(".vat input");
+
+      var netAMT = parseFloat($netInput.val().replace(/,/g, "")) || 0;
+      var isUserEdited = $vatInput.data("isUserEdited");
+
+      if (!isUserEdited) {
         var vat = netAMT * 0.16;
-        
-        $(e).find(".vat input").val(GO.util.numberWithCommas(vat.toFixed(2)));
+        $vatInput.val(GO.util.numberWithCommas(vat.toFixed(2)));
       }
     });
-  },
+  };
+})();
