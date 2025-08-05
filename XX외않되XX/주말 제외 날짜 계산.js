9c403113e6f680e130f5599275d15e9f73f5884e
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-08-05 09:39:12 +0900

    Update 주말 제외 날짜 계산.js

--- XX외않되XX/주말 제외 날짜 계산.js
+++ XX외않되XX/주말 제외 날짜 계산.js
@@ -8,6 +8,18 @@
       var startDate = moment($('.day_period input').eq(0).val(), "YYYY-MM-DD");
       var endDate = moment($('.day_period input').eq(1).val(), "YYYY-MM-DD");
       var dayDiff = endDate.diff(startDate, 'days') + 1;
+      var current = startDate.clone();
+
+      while (current.isSameOrBefore(endDate)) {
+        var dayOfWeek = current.day(); // 0: 일요일, 6: 토요일
+
+        // 주말이 아니고 공휴일이 아니면 근무일로 카운트
+        if (dayOfWeek == 0 || dayOfWeek == 6) {
+          dayDiff--;
+        }
+
+        current.add(1, 'days');
+      }
 
       // 유효하지 않은 날짜인 경우
       if (!startDate.isValid() || !endDate.isValid() || startDate > endDate || startDate.isAfter(endDate)) {
@@ -18,10 +30,8 @@
         let price = 0;
 
         if (["주임", "대리", "과장", "차장"].includes(selectedUserRank)) {
-          // price = 30000;
           price = 30000 * dayDiff;
         } else if (selectedUserRank === "팀장") {
-          // price = 35000;
           price = 35000 * dayDiff;
         } else if (["실장", "위원"].includes(selectedUserRank)) {
           price = 40000 * dayDiff;
@@ -32,3 +42,16 @@
         $(e).find(".price4").text(price ? GO.util.numberWithCommas(price) : "");
       }
     });
+
+    // 총 합계 계산
+    var sum_price4 = 0;
+
+    $(".price4").each(function () {
+      var val = parseFloat($(this).text().replace(/,/g, ""));
+      if (!isNaN(val)) {
+        sum_price4 += val;
+      }
+    });
+
+    $(".sum_price4").text(GO.util.numberWithCommas(sum_price4));
+  },
