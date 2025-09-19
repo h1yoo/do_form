190273e3935612c3217772bbadf7c8e9e4b9cb92
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-09-19 15:14:41 +0900

    Update 각 합계의 합계 오류.js

--- XX외않되XX/각 합계의 합계 오류.js
+++ XX외않되XX/각 합계의 합계 오류.js
@@ -1,8 +1,22 @@
-$('.price').on('input', function () {
-    let value = $(this).val().replace(/[^0-9]/g, ''); // 숫자만 남김
-    if (value) {
-        $(this).val(Number(value).toLocaleString()); // 5,000
-    } else {
-        $(this).val('');
-    }
-});
+
+    // 상품화내역, 수리, 탁송, 기타
+    $(".commPrice input, .repairPrice input, .consPrice input, .etcPrice input").on('change', function(){
+      self.calEstCost();
+    });
+
+
+    // 추정 비용 합계, 추정 부대비용 구하는 함수
+    calEstCost : function(){
+        var commPrice = parseInt($(".sum_commPriceVal").text().replace(/,/g, "")); if (isNaN(commPrice)) commPrice = 0;
+        var repairPrice = parseInt($(".sum_repairPriceVal").text().replace(/,/g, "")); if (isNaN(repairPrice)) repairPrice = 0;
+        var consPrice = parseInt($(".sum_consPriceVal").text().replace(/,/g, "")); if (isNaN(consPrice)) consPrice = 0;
+        var etcPrice = parseInt($(".sum_etcPriceVal").text().replace(/,/g, "")); if (isNaN(etcPrice)) etcPrice = 0;
+        var estCost = commPrice + repairPrice + consPrice + etcPrice;
+        console.log("commPrice: ", commPrice);
+        console.log("repairPrice: ", repairPrice);
+        console.log("consPrice: ", consPrice);
+        console.log("etcPrice: ", etcPrice);
+        console.log("estCost: ", estCost);
+  
+        $(".estCostVal").text(GO.util.numberWithCommas(estCost));
+    },
