257b668d286371c71380585daa88bf23f92ca05d
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-09-19 15:01:37 +0900

    Update 각 합계의 합계 오류.js

--- XX외않되XX/각 합계의 합계 오류.js
+++ XX외않되XX/각 합계의 합계 오류.js
@@ -1,13 +1,8 @@
-
-    // 추정 비용 합계, 추정 부대비용 구하는 함수
-    calEstCost : function(){
-        var commPrice = parseInt($(".sum_commPriceVal").text().replace(/,/g, "")) || 0;
-        var repairPrice = parseInt($(".sum_repairPriceVal").text().replace(/,/g, "")) || 0;
-        var consPrice = parseInt($(".sum_consPriceVal").text().replace(/,/g, "")) || 0;
-        var etcPrice = parseInt($(".sum_etcPriceVal").text().replace(/,/g, "")) || 0;
-        var estCost = commPrice + repairPrice + consPrice + etcPrice;
-  
-        $(".estCostVal").text(GO.util.numberWithCommas(estCost));
-    },
-
-결과 : 1,000,010,000
+$('.price').on('input', function () {
+    let value = $(this).val().replace(/[^0-9]/g, ''); // 숫자만 남김
+    if (value) {
+        $(this).val(Number(value).toLocaleString()); // 5,000
+    } else {
+        $(this).val('');
+    }
+});
