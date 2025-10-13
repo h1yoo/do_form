3d9a10d241936ad94f416d8600573a260898013b
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-10-13 13:14:58 +0900

    Update 날짜가 안 가져와짐.js

--- XX외않되XX/날짜가 안 가져와짐.js
+++ XX외않되XX/날짜가 안 가져와짐.js
@@ -1,6 +1,19 @@
+autoSubject: function() {
+  var draftDate = this.docModel.draftDate;
+  console.log("draftDate : ", draftDate);
 
-  autoSubject: function() {
-    var draftDate = this.docModel.draftDate;
-    console.log("draftDate : ", draftDate);
-    $('#subject').val(draftDate);
-  },
+  if (draftDate) {
+    var dateOnly = draftDate.replace(/\(.*\)/, '').trim(); // "2025-10-13"
+    var dateObj = new Date(dateOnly);
+    
+    if (!isNaN(dateObj.getTime())) {
+      var days = ['일', '월', '화', '수', '목', '금', '토'];
+      var formatted = `${dateObj.getFullYear()}년 ${dateObj.getMonth() + 1}월 ${dateObj.getDate()}일 (${days[dateObj.getDay()]})`;
+      $('#subject').val(formatted);
+    } else {
+      console.warn("유효하지 않은 날짜 형식:", dateOnly);
+    }
+  } else {
+    console.warn("draftDate 값이 없습니다.");
+  }
+}
