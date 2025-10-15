7285165d1080e684e8c63d735d33b85e525a410e
hyewon yoo <117055681+h1yoo@users.noreply.github.com>
2025-10-15 21:34:14 +0900

    Delete XX외않되XX directory

--- XX외않되XX/펀드명별계좌정보.js
+++
@@ -1,8 +0,0 @@
-
-      $(".selectEtc select").on("change", function () {
-        var selectEtc = $(this).val();
-                          
-        if(selectEtc == "기타") {
-          $(this).closest("td").find('.CBRE_defaultInput').show();
-        } 
-      });
